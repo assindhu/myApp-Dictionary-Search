354f82540cebb9ee1afcba75512308bdb7339621
assindhu <92337472+assindhu@users.noreply.github.com>
2022-06-27 21:53:11 +0200

    added search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
-
+import Dictionary from "./Dictionary";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Dictionary</header>
+    <div className="container">
+      <div className="App">
+        <header className="App-header">Dictionary</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="footer">
+          <a href="https://github.com/assindhu/myApp-Dictionary-Search">
+            Coded by sindhu
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
