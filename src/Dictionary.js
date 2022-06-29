d4c7c03fa0e4372af375e391b1b54a3702d72417
assindhu <92337472+assindhu@users.noreply.github.com>
2022-06-30 00:04:45 +0200

    added meanings and definitions.

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 import React, { useState } from "react";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data);
+    setResults(response.data[0]);
   }
   function search(event) {
     event.preventDefault();
@@ -23,6 +25,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" autoFocus={true} onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
